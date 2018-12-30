a43d53f492320434fe6cdf44e68afa433fc85117
zhenxindefeng@126.com <Xiehaijun945>
2018-12-31 00:58:48 +0800

    初始化项目

---
+++ src/main/java/com/haijun/study/springeasyui/SpringEasyuiApplication.java
@@ -0,0 +1,14 @@
+package com.haijun.study.springeasyui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEasyuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEasyuiApplication.class, args);
+    }
+
+}
+
